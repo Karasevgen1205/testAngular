751416e0415a66b793f25d75a3d76eb264abd609
Yauheni Karas <yauhenikaras@ester.co>
2021-08-30 18:41:37 +0300

    fix videojs and video scss

--- resources/scripts/shared/one-video.js
+++ resources/scripts/shared/one-video.js
@@ -6,34 +6,100 @@ export default class OneVideo {
   }
 
   init() {
-    console.log("video-ok");
-    const player = videojs("vid1", { controls: true, preload: "metadata" });
-    player.src({ type: "video/mp4", src: "resources/video/JS_Udemy_1-3.mp4" });
-    player.poster("resources/images/call.png");
-
-    const start = document.querySelector(".start");
-    start.addEventListener("click", () => {
-      player.ready(function () {
-        player.play();
-        console.log(player.duration());
+    // console.log("video-ok");
+    const manyVideo = document.querySelectorAll(".one-video__video");
+    manyVideo.forEach((oneVideo) => {
+      const player = videojs(oneVideo, {
+        controls: true,
+        preload: "metadata",
+        // userActions: {
+        //  doubleClick: true, //полноэкранныйрежим по двойному склику
+        // },
       });
-    });
-
-    const pause = document.querySelector(".pause");
-    pause.addEventListener("click", () => {
-      player.ready(function () {
-        player.pause();
-        console.log(player.currentTime());
+      player.src({
+        type: "video/mp4",
+        src: "resources/video/less.mp4" + "#t=150",
       });
-    });
+      // player.poster("resources/images/call.png");
+      player.aspectRatio("16:9");
 
-    // let long = player.duration();
-    // console.log(long);
-    // , function () {
-    //   this.addClass("video-ready");
-    // }
-    // );
-    // player.options.autoplay = true;
-    // videojs.options.controls = true;
+      // player.liveui(true);
+
+      player.one("play", function () {
+        this.currentTime(0);
+        oneVideo.classList.remove("one-video__change-poster");
+      });
+
+      player.on("play", function () {
+        // oneVideo.classList.remove("one-video__change-poster");
+        // console.log("play");
+      });
+
+      player.on("pause", function () {
+        // oneVideo.classList.add("one-video__change-poster");
+        // console.log("pause");
+      });
+
+      const l = document.querySelector(".vjs-play-control");
+      l.addEventListener("click", () => {
+        // oneVideo.classList.toggle("one-video__change-poster");
+      });
+
+      // oneVideo.addEventListener("click", (event) => {
+      // let target = event.target;
+      // if (target && target.classList.contains("vjs-big-play-button")) {
+      //     const btns = document.querySelectorAll(".vjs-playing");
+      //     btns.forEach((item, i) => {
+      //       if (target == item) {
+      //         oneVideo.classList.remove("one-video__change-poster");
+      // console.log("PK");
+      //       }
+      //     });
+      // }
+      // });
+
+      // const play = document.querySelector(".vjs-playing");
+      // play.addEventListener("click", () => {
+      //   oneVideo.classList.remove("one-video__change-poster");
+      // });
+
+      // const paused = document.querySelector(".vjs-paused");
+      // paused.addEventListener("click", () => {
+      //   oneVideo.classList.add("one-video__change-poster");
+      // });
+      // const start = document.querySelector(".start");
+      // start.addEventListener("click", () => {
+      //   player.ready(function () {
+      //     player.play();
+      //     oneVideo.classList.remove("one-video__change-poster");
+      //     console.log(player.duration());
+      //   });
+      // });
+
+      // const pause = document.querySelector(".pause");
+      // pause.addEventListener("click", () => {
+      //   player.ready(function () {
+      //     player.pause();
+      // oneVideo.classList.add("one-video__change-poster");
+      //     console.log(player.currentTime());
+      //   });
+      // });
+
+      const fullScreen = document.querySelector(".full-screen");
+      fullScreen.addEventListener("click", () => {
+        player.ready(() => {
+          player.requestFullscreen();
+        });
+      });
+
+      // let long = player.duration();
+      // console.log(long);
+      // , function () {
+      //   this.addClass("video-ready");
+      // }
+      // );
+      // player.options.autoplay = true;
+      // videojs.options.controls = true;
+    });
   }
 }
